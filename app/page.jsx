55ace43b36c58307032df7bf10f570a7cb375d9c
S-Moreira06 <moreira.s@outlook.com>
2026-01-22 11:31:01 +0100

    mise en place des components

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 import { Gravitas_One , Momo_Signature} from "next/font/google";
-import StackIcon from "tech-stack-icons";
+import Stacks from "@/components/Stacks";
 import Image from "next/image";
 import HeroImage from '../public/hero.png';
 
@@ -22,76 +22,7 @@ export default function Home() {
       <div className={gravitas_One.className} >
         <p className="text-sans text-xl text-center p-10">Tombé dans la marmite du dévellopement en 2020, je me suis formé (seul et grâce a plusieurs cursus de formation) durant tout se temps a comprendre le dev, travailler ma capacitée d'apprentissage et d'adaptation entre autres. J'ai pu aussi experimenter le travail en équipe et les méthodes agiles lors de ma dernière alternance.</p>
       </div>
-
-      <div className="p-10 bg-fuchsia-950">
-
-        <h2 className="text-4xl text-center">Stacks :</h2>
-
-        <div className="flex mb-5">
-          <h3 className="self-center text-2xl pr-20">Natifs</h3>
-          <div className="flex gap-10">
-            
-            <div style={{ width: 90, height: 90 }}>
-              <StackIcon name="html5" />
-            </div>
-            <div style={{ width: 90, height: 90 }}>
-              <StackIcon name="css3" />
-            </div>
-            <div style={{ width: 90, height: 90 }}>
-              <StackIcon name="js" />
-            </div>
-            <div style={{ width: 90, height: 90 }}>
-              <StackIcon name="php" />
-            </div>
-            
-          </div>
-        </div>
-
-        <div className="flex justify-end">
-          <div className="flex gap-10">
-            <div style={{ width: 90, height: 90 }}>
-              <StackIcon name="react" />
-            </div>
-            <div style={{ width: 90, height: 90 }}>
-              <StackIcon name="nextjs" />
-            </div>
-            <div style={{ width: 90, height: 90 }}>
-              <StackIcon name="laravel" />
-            </div>
-            <div style={{ width: 90, height: 90 }}>
-              <StackIcon name="hono" />
-            </div>
-            <div style={{ width: 90, height: 90 }}>
-              <StackIcon name="mysql" />
-            </div>
-            <div style={{ width: 90, height: 90 }}>
-              <StackIcon name="expo" />
-            </div>
-            <div style={{ width: 90, height: 90 }}>
-              <StackIcon name="nodejs" />
-            </div>
-            <div style={{ width: 90, height: 90 }}>
-              <StackIcon name="bootstrap4" />
-            </div>
-            <h3 className="self-center text-2xl pr-20">Library and framework</h3>
-
-          </div>
-        </div>
-        <div className="flex gap-5">
-          
-          <div style={{ width: 90, height: 90 }}>
-            <StackIcon name="git" />
-          </div>
-          <div style={{ width: 90, height: 90 }}>
-            <StackIcon name="github" />
-          </div>
-          
-  
-
-
-        </div>
-      </div>
-
+      <Stacks/>
       <div className="grid grid-cols-2 p-10">
         <div>
           Projet 1 : Chat Interactif
